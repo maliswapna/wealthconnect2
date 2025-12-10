8f50cdfa196af731b7d5cae363341b98cb5c0e04
maliswapna <swapna1.sfdc@gmail.com>
2025-12-09 21:50:12 -0600

    message

--- test/testbutton.spec.js
+++ test/testbutton.spec.js
@@ -1,23 +1,23 @@
-// login.spec.js
-const { test, expect } = require('@playwright/test');
+// // login.spec.js
+// const { test, expect } = require('@playwright/test');
 
-test('Login Test - Saucedemo', async ({ page }) => {
+// test('Login Test - Saucedemo', async ({ page }) => {
 
-  // 1. Open URL
-  await page.goto('https://www.saucedemo.com/');
+//   // 1. Open URL
+//   await page.goto('https://www.saucedemo.com/');
 
-  // 2. Enter username
-  await page.fill('#user-name', 'standard_user');
+//   // 2. Enter username
+//   await page.fill('#user-name', 'standard_user');
 
-  // 3. Enter password
-  await page.fill('#password', 'secret_sauce');
+//   // 3. Enter password
+//   await page.fill('#password', 'secret_sauce');
 
-  // 4. Click Login
-  await page.click('#login-button');
+//   // 4. Click Login
+//   await page.click('#login-button');
 
-  //6 wait and verify successful login
-  await expect(page.locator('.inventory_list')).toBeVisible();
+//   //6 wait and verify successful login
+//   await expect(page.locator('.inventory_list')).toBeVisible();
 
-  // 6. Verify URL
-  await expect(page).toHaveURL(/inventory/);
-});
+//   // 6. Verify URL
+//   await expect(page).toHaveURL(/inventory/);
+// });
